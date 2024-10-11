678ede3e9044ab528c4e2aba7e8ad0079703daa3
Angelo <yapangeloterezo@gmail.com>
2024-10-11 05:42:33 -0700

    about and contact page done

--- src/components/NavPopup/NavPopup.jsx
+++ src/components/NavPopup/NavPopup.jsx
@@ -1,20 +1,24 @@
 import "./NavPopup.scss";
 import { Link } from "react-router-dom";
 
-const NavPopup = () => {
+const NavPopup = ({ closePopup }) => {
+  const handleLinkClick = () => {
+    closePopup();
+  };
+
   return (
     <nav className="navpopup">
       <div className="navpopup__page-container">
-        <Link to={"/"}>
+        <Link to={"/"} onClick={handleLinkClick}>
           <h1 className="navpopup__page">HOME</h1>
         </Link>
-        <Link to={"/services"}>
+        <Link to={"/services"} onClick={handleLinkClick}>
           <h1 className="navpopup__page">SERVICES</h1>
         </Link>
-        <Link to={"/about"}>
+        <Link to={"/about"} onClick={handleLinkClick}>
           <h1 className="navpopup__page">ABOUT</h1>
         </Link>
-        <Link to={"/contact"}>
+        <Link to={"/contact"} onClick={handleLinkClick}>
           <h1 className="navpopup__page">CONTACT</h1>
         </Link>
       </div>
--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -2,9 +2,56 @@ import "./About.scss";
 
 const About = () => {
   return (
-    <div className="about">
-      <h1>about</h1>
-    </div>
+    <>
+      <div className="about">
+        <h1 className="about__title">ABOUT ME</h1>
+      </div>
+      <div className="about__container">
+        <p className="about__paragraph">
+          Hi there! I am Bernadette, a E-RYT 500 yoga teacher, Yoga Therapist
+          C-AIYT candidate, a holistic nutritionist, a trained wellness coach,
+          and a reiki practitioner. 
+        </p>
+        <p className="about__paragraph">
+          All that just means I’m passionate about what I do. Fourteen years
+          ago, I took my first yoga class and have never looked back since. It
+          didn’t just improve my overall outlook in life, but it also played a
+          significant role in shifting my eating habits. The more I practiced,
+          the more I became mindful of what I put in my body, which led me to
+          become a holistic nutritionist. My belief in the power of the
+          plant-based diet to heal led me to become vegan initially for health.
+          My purpose then evolved to doing it for the animals, and then for the
+          planet.
+        </p>
+        <p className="about__paragraph">
+          With these conscious choices, I saw the benefits of attaining peace,
+          clarity, and an overall feeling of calm as I navigated life. Driven by
+          my love for service, connecting with people, and being part of a
+          community, I expanded my skill set to deepen my understanding of
+          health and wellness.
+        </p>
+        <p className="about__paragraph">
+          I am currently completing my practicum in Yoga Therapy at the
+          Vancouver School of Healing Arts. This program continues to deepen my
+          ability to support your healing journey using a variety of therapeutic
+          yoga tools. As I continue to grow as a yoga teacher, wellness
+          advocate, and individual, I am more inspired than ever to serve you
+          through the transformative practice of mindfulness.
+        </p>
+        <p className="about__paragraph">
+          I am especially passionate about supporting the community,
+          particularly seniors, as they seek ease and comfort during this new
+          chapter of life.
+        </p>
+        <p className="about__paragraph">
+          My mission is to share everything I’ve learned with you and show you
+          how to access the heightened awareness that is already within you. 
+        </p>
+        <p className="about__paragraph">
+          Let’s explore together ways to achieve a mindful lifestyle.
+        </p>
+      </div>
+    </>
   );
 };
 
