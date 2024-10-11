37df03890347b958061e6a9792931c1faaa71c3a
Angelo <yapangeloterezo@gmail.com>
2024-10-11 04:35:37 -0700

    homepage mobile done

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,21 +1,106 @@
 import "./Home.scss";
 import HeroSliderMobile from "../../components/HeroSliderMobile/HeroSliderMobile";
 import Button from "../../components/Button/Button";
+import { Link } from "react-router-dom";
+import TextBox from "../../components/TextBox/TextBox";
+import { useState } from "react";
+import PropsPopup from "../../components/PropsPopup/PropsPopup";
+import {
+  credentialsData,
+  yogaEducationData,
+  careerHighlightsData,
+} from "../../scripts/props-data";
 
 const Home = () => {
+  const [visiblePopup, setVisiblePopup] = useState(null);
+
+  const handleOpenPopup = (popupType) => {
+    setVisiblePopup(popupType);
+  };
+
+  const handleClosePopup = () => {
+    setVisiblePopup(null);
+  };
+
   return (
     <div className="home">
-      <HeroSliderMobile />
-      <div className="home__hero">
-        <h1 className="home__tagline">WHERE MOVEMENT MEETS MINDFULNESS</h1>
-        <Button text="Start Your Journey" className="button--primary" />
-      </div>
+      <section className="home__hero">
+        <div className="home__hero-text-container">
+          <h1 className="home__hero-tagline">
+            WHERE MOVEMENT MEETS MINDFULNESS
+          </h1>
+          <Link to={"/services"}>
+            <Button text="Start Your Journey" className="button--primary" />
+          </Link>
+        </div>
+        <HeroSliderMobile className="home__hero-background" />
+      </section>
+
       <section className="home__intro">
         <div className="home__intro-text-container">
           <h1 className="home__intro-greeting1">Hi There!</h1>
           <h1 className="home__intro-greeting2">I'm Bernadette</h1>
+          <TextBox
+            className={"textbox home__intro-textbox"}
+            paragraphs={[
+              "I'm a passionate 500-hour yoga teacher, holistic nutritionist, and wellness coach.",
+              "Through yoga, I found mindfulness and improved my eating habits, inspiring me to become a nutritionist. My journey led me to embrace a plant-based diet for health, animals, and the planet.",
+              "As a Yoga Therapist C-AIYT candidate, I'm equipped to support your healing journey. I strive to serve by sharing my knowledge and promoting mindfulness. Join me to discover a mindful lifestyle.",
+            ]}
+          />
+          <Button
+            text="More About Me"
+            className="home__intro-button button--primary"
+          />
         </div>
       </section>
+
+      <section className="home__creds">
+        <div className="home__creds-container">
+          <h1
+            className="home__creds-item"
+            onClick={() => handleOpenPopup("credentials")}
+          >
+            Credentials
+          </h1>
+          <h1
+            className="home__creds-item"
+            onClick={() => handleOpenPopup("yogaEducation")}
+          >
+            Yoga Education
+          </h1>
+          <h1
+            className="home__creds-item"
+            onClick={() => handleOpenPopup("careerHighlights")}
+          >
+            Career Highlights
+          </h1>
+        </div>
+      </section>
+
+      {visiblePopup === "credentials" && (
+        <PropsPopup
+          title="Credentials"
+          content={credentialsData}
+          onClose={handleClosePopup}
+        />
+      )}
+
+      {visiblePopup === "yogaEducation" && (
+        <PropsPopup
+          title="Yoga Education"
+          content={yogaEducationData}
+          onClose={handleClosePopup}
+        />
+      )}
+
+      {visiblePopup === "careerHighlights" && (
+        <PropsPopup
+          title="Career Highlights"
+          content={careerHighlightsData}
+          onClose={handleClosePopup}
+        />
+      )}
     </div>
   );
 };
---
+++ src/scripts/props-data.js
@@ -0,0 +1,98 @@
+export const credentialsData = [
+  {
+    cred: "240-HR Advanced Yoga Therapy",
+    location: "VSOHA, Vancouver, Canada",
+  },
+  { cred: "Restorative Yoga Certified", location: "with Tianne Allen" },
+  {
+    cred: "300-HR Therapeutic Yoga Teacher",
+    location: "VSOHA, Vancouver, Canada",
+  },
+  {
+    cred: "500-HR Yoga Certified",
+    location: "YogaWorks New York, USA",
+  },
+  {
+    cred: "Yin Yoga Certified",
+    location: "With Bernie Clarke, Vancouver, Canada",
+  },
+  {
+    cred: "Restorative Yoga Certified",
+    location: "Semperviva, Vancouver, Canada",
+  },
+  {
+    cred: "Baptiste Yoga Training",
+    location: "Vancouver, Canada",
+  },
+  {
+    cred: "Yoga Teacher Trainer",
+    location: "YogaHub, Cebu, Philippines",
+  },
+  {
+    cred: "Registered Holistic Nutritionist",
+    location: "Canadian School of Natural Nutrition, Vancouver, Canada",
+  },
+  {
+    cred: "The Plant-fed Gut Course",
+    location: "With Dr. Will Bulsiewicz",
+  },
+  {
+    cred: "Trained Life Coach",
+    location: "Rhodes Wellness College, Vancouver, Canada",
+  },
+  {
+    cred: "Bachelor in Science in Business Management",
+    location: "Cebu Philippines",
+  },
+  {
+    cred: "Six Sigma Black Belt",
+    location: "Sykes Enterprises, Inc.",
+  },
+];
+
+export const yogaEducationData = [
+  {
+    cred: "200-hr Teacher Trainer Assistant",
+    location: "New York, USA",
+  },
+  {
+    cred: "200-hr Teacher Trainer Assistant",
+    location: "Saigon, Vietnam",
+  },
+  {
+    cred: "Mentorship",
+    location: "With Rachel Scott, Vancouver, CA",
+  },
+  {
+    cred: "Over 50hrs of Various Yoga Workships",
+    location: "Philippines, Hongkong, New York, Vancouver",
+  },
+];
+
+export const careerHighlightsData = [
+  {
+    cred: "Director",
+    location: "Teacher Training and Development",
+  },
+  {
+    cred: "Yoga Teacher",
+    location: "BC Stadium, in partnership with Mat Collective and BC Lions",
+  },
+  {
+    cred: "Guest Speaker",
+    location: `Full Tank, With Bro. Bo Sanchez, "How To Shift To A Healthier Diet"`,
+  },
+  {
+    cred: "Speaker",
+    location: `The Feast Grand Conference, "Faith and Fitness: Connection Between Mind, Body and Spirit"`,
+  },
+  {
+    cred: "Panel Speaker",
+    location: `Infinite Care, "Wellness At Home"`,
+  },
+  {
+    cred: "Co-founder",
+    location:
+      "Ubuntu Festival - Yoga, Art, Food, Music and Love Wellness Retreat, Cebu, Philippines",
+  },
+];
