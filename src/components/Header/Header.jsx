d0cabbfdf5821f3865ac5d35e41501e19030cbb1
Angelo <yapangeloterezo@gmail.com>
2024-10-11 05:07:50 -0700

    services page done

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -4,6 +4,7 @@ import logo from "../../assets/byoga_logo.png";
 import hamburger from "../../assets/hamburger.svg";
 import close from "../../assets/close.svg";
 import NavPopUp from "../NavPopup/NavPopup";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   const [isNavVisible, setIsNavVisible] = useState(false);
@@ -24,8 +25,10 @@ const Header = () => {
         className={`header__hamburger ${isNavVisible ? "open" : ""}`}
         onClick={handleHamburgerClick}
       />
-      <img src={logo} alt="logo" className="header__logo" />
-      {isNavVisible && <NavPopUp onClose={handleCloseNav} />}
+      <Link to={"/"}>
+        <img src={logo} alt="logo" className="header__logo" />
+        {isNavVisible && <NavPopUp onClose={handleCloseNav} />}
+      </Link>
     </header>
   );
 };
--- src/pages/Services/Services.jsx
+++ src/pages/Services/Services.jsx
@@ -3,7 +3,19 @@ import "./Services.scss";
 const Services = () => {
   return (
     <div className="services">
-      <h1>services</h1>
+      <section className="services__yoga">
+        <div className="services__yoga-container">
+          <h1 className="services__yoga-title">YOGA</h1>
+          <h2 className="services__yoga-types">YOGA THERAPY</h2>
+          <h2 className="services__yoga-types">TEACHER TRAINING</h2>
+          <h2 className="services__yoga-types">STUDIO CLASSES</h2>
+        </div>
+      </section>
+      <section className="services__coaching">
+        <div className="services__coaching-container">
+          <h1 className="services__coaching-title">WELLNESS COACHING</h1>
+        </div>
+      </section>
     </div>
   );
 };
